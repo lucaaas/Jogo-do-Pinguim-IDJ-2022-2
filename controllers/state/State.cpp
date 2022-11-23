adc647c52d1644c9a943c39ad3eecc99bcd0dcdb
lucaaas <lucasoliveira2306@gmail.com>
2022-11-23 20:34:35 -0300

    Adicionado musica inicial.

--- controllers/state/State.cpp
+++ controllers/state/State.cpp
@@ -6,13 +6,18 @@
 #include "State.h"
 #include "../../helpers/asset/AssetHelper.h"
 
-State::State() : bg(AssetHelper::GetAssetPath((char *) "img", "ocean.jpg")) {
+State::State() : bg(AssetHelper::GetAssetPath((char *) "img", "ocean.jpg")),
+                 music(AssetHelper::GetAssetPath((char *) "audio", "stageState.ogg")) {
     quitRequested = false;
+
+    music.Play();
 }
 
 void State::LoadAssets() {
     bg = Sprite(AssetHelper::GetAssetPath((char *) "img", "ocean.jpg"));
+
     music = Music(AssetHelper::GetAssetPath((char *) "audio", "stageState.ogg"));
+    music.Play();
 }
 
 bool State::QuitRequested() const {
